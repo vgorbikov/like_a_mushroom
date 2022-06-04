209ae8c4b27471d0e08de06d9e0ea0e98e263a3e
Vadim <V.gorb-123456789@yandex.ru>
2022-06-05 00:19:02 +0300

    Теперь игрок тоже объект. Перемещение объектов вынесено в  функции

--- like_a_mushroom/src/like_a_mushroom.c
+++ like_a_mushroom/src/like_a_mushroom.c
@@ -31,14 +31,18 @@ SDL_Window *initWindow(int WIDTH, int HEIGHT)
 	return window;
 }
 
+typedef struct ObjTextureSet
+{
+	SDL_Texture *texture;
+	SDL_Rect *tex_box;
+}OTexSet;
+
 typedef struct Object
 {
 	SDL_Rect box;
 	float x_speed, y_speed, x_boost, y_boost, weight;
 	SDL_bool monolith;
 	SDL_Texture *texture;
-	struct Object *prev_object_nearby;
-	struct Object *next_object_nearby; //указатель на объект, находящийся с данным на одном поле
 } Obj;
 
 
@@ -52,56 +56,158 @@ Obj *initObject(SDL_Rect obj_box, SDL_Texture *tex, float weight, SDL_bool monol
 	new_object->y_speed = 0;
 	new_object->y_boost = 0;
 	new_object->texture = tex;
-	new_object->prev_object_nearby = NULL;
-	new_object->next_object_nearby = NULL;
 	return new_object;
 }
 
-typedef struct MapField
+typedef struct ObjListElem
 {
-	Obj *head;
-	Obj *current;
-}MapField;
+	Obj *object;
+	struct ObjListElem *next;
+	struct ObjListElem *prev;
+}OLE;
 
-
-MapField *initMapField()
+OLE *initOLE(Obj *obj)
 {
-	MapField *new_map = malloc(sizeof(MapField));
-	new_map->head = NULL;
-	new_map->current = NULL;
-	return new_map;
+	OLE *returned = malloc(sizeof(OLE));
+	returned->object = obj;
+	returned->next = NULL;
+	returned->prev = NULL;
+	return returned;
 }
 
-int addObj(MapField *map, Obj *obj)
+typedef struct Objlist
 {
-	if(map->head == NULL)
+	OLE *head;
+	OLE *current;
+}ObjList;
+
+
+ObjList *initObjList()
+{
+	ObjList *new_list = malloc(sizeof(ObjList));
+	new_list->head = NULL;
+	new_list->current = NULL;
+	return new_list;
+}
+
+int addObjInList(ObjList *list, Obj *obj)
+{
+	OLE *elem = initOLE(obj);
+	if(list->head == NULL)
 	{
-		map->head = obj;
-		map->current = obj;
+		list->head = elem;
+		list->current = elem;
 		return 0;
 	}
-	if(map->current->next_object_nearby != NULL) obj->next_object_nearby = map->current->next_object_nearby;
-	map->current->next_object_nearby = obj;
-	obj->prev_object_nearby = map->current;
-	map->current = obj;
+	if(list->current->next != NULL)
+	{
+		elem->next = list->current->next;
+		list->current->next->prev = elem;
+	}
+	list->current->next = elem;
+	elem->prev = list->current;
+	list->current = elem;
 	return 0;
 }
 
-void nextObj(MapField *map)
+void nextObjInList(ObjList *list)
 {
-	if(map->current != NULL) map->current = map->current->next_object_nearby;
+	if(list->current != NULL) list->current = list->current->next;
 }
 
-void headObj(MapField *map)
+void headObjInList(ObjList *list)
 {
-	map->current = map->head;
+	list->current = list->head;
+}
+
+int delObjFromList(ObjList *list)
+{
+	//printf("Deep deleting\n");
+	if(list->current != NULL)
+	{
+		if(list->current->prev != NULL) list->current->prev->next = list->current->next;
+		if(list->current->next != NULL) list->current->next->prev = list->current->prev;
+		if(list->current == list->head) list->head = list->current->next;
+		OLE *new_cur = list->current->next;
+		if(list->current != NULL) free(list->current);
+		list->current = new_cur;
+	//printf("Deep deleting Done\n");
+	}
+	return 0;
+}
+
+int delObjList(ObjList *list)
+{
+	headObjInList(list);
+	while(list->head != NULL) delObjFromList(list);
+	if(list != NULL) free(list);
+	return 0;
 }
 
 typedef struct Map
 {
 	int height, width;
-	MapField *fields;
+	ObjList *tiles;
 } Map;
+
+void addObjInMap(Map *map, Obj *obj)
+{
+	if(obj->box.x < 0) obj->box.x = 0;
+	if(obj->box.x > (map->width*BLOCK_SIZE - obj->box.w)) obj->box.x = (map->width*BLOCK_SIZE - obj->box.w);
+	if(obj->box.y < 0) obj->box.y = 0;
+	if(obj->box.y > (map->height*BLOCK_SIZE - obj->box.h)) obj->box.y = (map->height*BLOCK_SIZE - obj->box.h);
+	for(int y = obj->box.y; y <= (obj->box.y + obj->box.h - 1); y += BLOCK_SIZE)
+	{
+		for(int x = obj->box.x; x <= (obj->box.x + obj->box.w - 1); x += BLOCK_SIZE)
+		{
+			int fx = x/BLOCK_SIZE;
+			int fy = y/BLOCK_SIZE;
+			//printf("Start add obj in tile %i\n", map->width*fy + fx);
+			addObjInList(&map->tiles[map->width*fy + fx], obj);
+		}
+	}
+	//printf("Added\n");
+}
+
+void delObjFromMap(Map *map, Obj *obj)
+{
+	for(int y = obj->box.y; y <= (obj->box.y + obj->box.h - 1); y += BLOCK_SIZE)
+		{
+			for(int x = obj->box.x; x <= (obj->box.x + obj->box.w - 1); x += BLOCK_SIZE)
+			{
+				int fx = x/BLOCK_SIZE;
+				int fy = y/BLOCK_SIZE;
+				//printf("Start deleting obj from field %i\n", map->width*fy + fx);
+				while(map->tiles[map->width*fy + fx].current->object != obj) nextObjInList(&map->tiles[map->width*fy + fx]);
+				delObjFromList(&map->tiles[map->width*fy + fx]);
+				headObjInList(&map->tiles[map->width*fy + fx]);
+			}
+		}
+	//printf("Deleted\n");
+}
+
+int objInList(ObjList *list, Obj *obj)
+{
+	while(list->current != NULL)
+	{
+		if(list->current->object == obj)
+		{
+			headObjInList(list);
+			return 1;
+		}
+		nextObjInList(list);
+	}
+	headObjInList(list);
+	return 0;
+}
+
+void moveObj(Map *map, Obj *obj, int dx, int dy)
+{
+	delObjFromMap(map, obj);
+	obj->box.x += dx;
+	obj->box.y += dy;
+	addObjInMap(map, obj);
+}
 
 Map *mapLoad(SDL_Renderer *rend)
 {
@@ -119,8 +225,8 @@ Map *mapLoad(SDL_Renderer *rend)
 		fread(height, 1, 1, map);
 		new_map->height = *height;
 		new_map->width = *width;
-		new_map->fields = calloc(width[0]*height[0], sizeof(MapField));
-		printf("Start_load: %i \n", new_map->width);
+		new_map->tiles = calloc(width[0]*height[0], sizeof(ObjList));
+		printf("Map length: %i \n", new_map->width);
 		for(int i=0;i<*height;i++)
 		{
 			for(int j=0;j<*width;j++)
@@ -137,7 +243,7 @@ Map *mapLoad(SDL_Renderer *rend)
 					SDL_Texture *tex = stone;
 					Obj *block = initObject(block_box, tex, 1, SDL_TRUE);
 					//printf("block %i\n", i*new_map->width+j);
-					addObj(&new_map->fields[i*new_map->width+j], block);
+					addObjInMap(new_map, block);
 					break;
 				case 255:
 					break;
@@ -150,12 +256,6 @@ Map *mapLoad(SDL_Renderer *rend)
 	return new_map;
 }
 
-void physCalc()
-{
-
-}
-
-
 int main(int argc, char *argv[]) {
 
 	setvbuf(stdout, NULL, _IONBF, 0);
@@ -163,26 +263,25 @@ int main(int argc, char *argv[]) {
 
 	SDL_Window *window = initWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
 	SDL_Renderer *rend = NULL;
-	SDL_Texture *player = NULL;
 	SDL_Texture *background = NULL;
 	SDL_bool loopShouldStop = SDL_FALSE;
 
 
 	rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	player = IMG_LoadTexture(rend, "textures/player_anim.png");
 	background = IMG_LoadTexture(rend, "textures/background.bmp");
 	Map *g_map = mapLoad(rend);
 
+
+	SDL_Texture *playerTex = IMG_LoadTexture(rend, "textures/player_anim.png");
 	SDL_Rect playerBox;
 	playerBox.x = 0;
 	playerBox.y = 0;
 	playerBox.h = 50;
 	playerBox.w = 50;
 
-	int xspeed = 0;
-	int yspeed = 0;
-	int steps = 0;
-	int frame = 0;
+	Obj *player = initObject(playerBox, playerTex, 1, SDL_FALSE);
+	addObjInMap(g_map, player);
+
 
 	while (!loopShouldStop)
 	{
@@ -195,55 +294,52 @@ int main(int argc, char *argv[]) {
 					loopShouldStop = SDL_TRUE;
 					break;
 				case SDL_KEYDOWN:
-					if(event.key.keysym.sym == SDLK_RIGHT) xspeed = 1;
-					if(event.key.keysym.sym == SDLK_LEFT) xspeed = -1;
-					if(event.key.keysym.sym == SDLK_UP) yspeed = 1;
-					if(event.key.keysym.sym == SDLK_DOWN) yspeed = -1;
+					if(event.key.keysym.sym == SDLK_RIGHT) player->x_speed = 1;
+					if(event.key.keysym.sym == SDLK_LEFT) player->x_speed = -1;
+					if(event.key.keysym.sym == SDLK_UP) player->y_speed = -1;
+					if(event.key.keysym.sym == SDLK_DOWN) player->y_speed = 1;
 					break;
 				case SDL_KEYUP:
-					xspeed = 0;
-					yspeed = 0;
-					steps = 0;
-					frame = 0;
+					player->x_speed = 0;
+					player->y_speed = 0;
 					break;
 			}
 		}
 
-		if(xspeed == 1)
+		if(player->x_speed == 1)
 		{
-			playerBox.x += 1;
-			steps +=1;
-			if(steps > BLOCK_SIZE*8) steps = 0;
-			frame = steps/(BLOCK_SIZE*8/6) + 1;
-		}
-		if(xspeed == -1) playerBox.x -= 1;
-		if(yspeed == 1) playerBox.y -= 1;
-		if(yspeed == -1) playerBox.y += 1;
+			moveObj(g_map, player, 1, 0);
 
-		SDL_Rect FrameBox;
-		FrameBox.x = 100*frame;
-		FrameBox.y = 0;
-		FrameBox.h = 100;
-		FrameBox.w = 100;
+		}
+		if(player->x_speed == -1) moveObj(g_map, player, -1, 0);
+		if(player->y_speed == 1) moveObj(g_map, player, 0, 1);
+		if(player->y_speed == -1) moveObj(g_map, player, 0, -1);
 
 		SDL_RenderClear(rend);
 		SDL_RenderCopy(rend, background, NULL, NULL);
 		//printf("Start render\n");
+		ObjList *writed = initObjList();
 		for(int i=0;i<g_map->height;i++)
 		{
 			for(int j=0;j<g_map->width;j++)
 			{
-				//printf("Block %i \n", i*g_map->width + j);
-				MapField f = g_map->fields[i*g_map->width + j];
-				if(f.head != NULL) SDL_RenderCopy(rend, f.current->texture, NULL, &f.current->box);
+				headObjInList(&g_map->tiles[i*g_map->width + j]);
+				while(g_map->tiles[i*g_map->width + j].current != NULL)
+				{
+					if(objInList(writed, g_map->tiles[i*g_map->width + j].current->object) == 0)
+					{
+						SDL_RenderCopy(rend, g_map->tiles[i*g_map->width + j].current->object->texture, NULL, &g_map->tiles[i*g_map->width + j].current->object->box);
+						addObjInList(writed, g_map->tiles[i*g_map->width + j].current->object);
+					}
+					nextObjInList(&g_map->tiles[i*g_map->width + j]);
+				}
+				headObjInList(&g_map->tiles[i*g_map->width + j]);
 			}
 		}
-		SDL_RenderCopy(rend, player, &FrameBox, &playerBox);
+		delObjList(writed);
 		SDL_RenderPresent(rend);
 		SDL_Delay(1);
 	}
-
-	SDL_DestroyTexture(player);
 	SDL_DestroyTexture(background);
 	SDL_DestroyRenderer(rend);
 	SDL_DestroyWindow(window);
