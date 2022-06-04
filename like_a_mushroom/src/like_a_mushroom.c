321cc143f48c6f74acba62bd1782ff658a372b21
Vadim <V.gorb-123456789@yandex.ru>
2022-06-04 12:00:10 +0300

    Тестируется новый способ обработки карты, эксперименты с анимацией

--- like_a_mushroom/src/like_a_mushroom.c
+++ like_a_mushroom/src/like_a_mushroom.c
@@ -35,77 +35,79 @@ typedef struct Object
 {
 	SDL_Rect box;
 	float x_speed, y_speed, x_boost, y_boost, weight;
+	SDL_bool monolith;
 	SDL_Texture *texture;
+	struct Object *prev_object_nearby;
+	struct Object *next_object_nearby; //указатель на объект, находящийся с данным на одном поле
 } Obj;
 
-typedef struct MapBlock
-{
-	SDL_Rect box;
-	SDL_Texture *texture;
-	SDL_bool is_death;
-	struct MapBlock *next;
-} MapBlock;
 
-Obj initObject(SDL_Rect obj_box, float weight)
+Obj *initObject(SDL_Rect obj_box, SDL_Texture *tex, float weight, SDL_bool monolith)
 {
-	Obj new_object;
-	new_object.box = obj_box;
-	new_object.weight = weight;
-	new_object.x_speed = 0;
-	new_object.x_boost = 0;
-	new_object.y_speed = 0;
-	new_object.y_boost = 0;
+	Obj *new_object = malloc(sizeof(Obj));
+	new_object->box = obj_box;
+	new_object->weight = weight;
+	new_object->x_speed = 0;
+	new_object->x_boost = 0;
+	new_object->y_speed = 0;
+	new_object->y_boost = 0;
+	new_object->texture = tex;
+	new_object->prev_object_nearby = NULL;
+	new_object->next_object_nearby = NULL;
 	return new_object;
 }
 
-MapBlock *initMapBlock(SDL_Rect block_box, SDL_Texture *texture, SDL_bool deth)
+typedef struct MapField
 {
-	MapBlock *new_object = calloc(1, sizeof(MapBlock));
-	new_object->box = block_box;
-	new_object->texture = texture;
-	new_object->is_death = deth;
-	new_object->next = NULL;
-	return new_object;
+	Obj *head;
+	Obj *current;
+}MapField;
+
+
+MapField *initMapField()
+{
+	MapField *new_map = malloc(sizeof(MapField));
+	new_map->head = NULL;
+	new_map->current = NULL;
+	return new_map;
+}
+
+int addObj(MapField *map, Obj *obj)
+{
+	if(map->head == NULL)
+	{
+		map->head = obj;
+		map->current = obj;
+		return 0;
+	}
+	if(map->current->next_object_nearby != NULL) obj->next_object_nearby = map->current->next_object_nearby;
+	map->current->next_object_nearby = obj;
+	obj->prev_object_nearby = map->current;
+	map->current = obj;
+	return 0;
+}
+
+void nextObj(MapField *map)
+{
+	if(map->current != NULL) map->current = map->current->next_object_nearby;
+}
+
+void headObj(MapField *map)
+{
+	map->current = map->head;
 }
 
 typedef struct Map
 {
-	struct MapBlock *head;
-	struct MapBlock *current;
-}Map;
-
-int addMapBlock(Map *map, MapBlock *block)
-{
-	if(map->head == NULL)
-	{
-		map->head = block;
-		map->current = block;
-	}
-	else
-	{
-		map->current->next = block;
-		map->current = block;
-	}
-	return 0;
-}
-
-int nextBlock(Map *map)
-{
-	if(map->current != NULL) map->current = map->current->next;
-	return 0;
-}
-
-int headBlock(Map *map)
-{
-	map->current = map->head;
-	return 0;
-}
+	int height, width;
+	MapField *fields;
+} Map;
 
 Map *mapLoad(SDL_Renderer *rend)
 {
 	FILE *map = fopen("maps/binmap.bin", "rb");
-	Map *new_map = calloc(1, sizeof(Map));
 	SDL_Texture *stone = IMG_LoadTexture(rend, "textures/stone.png");
+	Map *new_map = malloc(sizeof(Map));
 	if (map == NULL) printf("No open\n");
 	else
 	{
@@ -115,7 +117,10 @@ Map *mapLoad(SDL_Renderer *rend)
 
 		fread(width, 2, 1, map);
 		fread(height, 1, 1, map);
-		printf("Загружена карта %dx%d\n", width[0], height[0]);
+		new_map->height = *height;
+		new_map->width = *width;
+		new_map->fields = calloc(width[0]*height[0], sizeof(MapField));
+		printf("Start_load: %i \n", new_map->width);
 		for(int i=0;i<*height;i++)
 		{
 			for(int j=0;j<*width;j++)
@@ -130,8 +135,9 @@ Map *mapLoad(SDL_Renderer *rend)
 					block_box.h = BLOCK_SIZE;
 					block_box.w = BLOCK_SIZE;
 					SDL_Texture *tex = stone;
-					MapBlock *block = initMapBlock(block_box, tex, SDL_FALSE);
-					addMapBlock(new_map, block);
+					Obj *block = initObject(block_box, tex, 1, SDL_TRUE);
+					//printf("block %i\n", i*new_map->width+j);
+					addObj(&new_map->fields[i*new_map->width+j], block);
 					break;
 				case 255:
 					break;
@@ -139,10 +145,15 @@ Map *mapLoad(SDL_Renderer *rend)
 			}
 		}
 		fclose(map);
+		printf("Загружена карта %dx%d\n", width[0], height[0]);
 	}
 	return new_map;
 }
 
+void physCalc()
+{
+
+}
 
 
 int main(int argc, char *argv[]) {
@@ -158,18 +169,20 @@ int main(int argc, char *argv[]) {
 
 
 	rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	player = IMG_LoadTexture(rend, "textures/player.png");
+	player = IMG_LoadTexture(rend, "textures/player_anim.png");
 	background = IMG_LoadTexture(rend, "textures/background.bmp");
 	Map *g_map = mapLoad(rend);
 
 	SDL_Rect playerBox;
 	playerBox.x = 0;
 	playerBox.y = 0;
-	playerBox.h = 30;
-	playerBox.w = 30;
+	playerBox.h = 50;
+	playerBox.w = 50;
 
 	int xspeed = 0;
 	int yspeed = 0;
+	int steps = 0;
+	int frame = 0;
 
 	while (!loopShouldStop)
 	{
@@ -190,25 +203,42 @@ int main(int argc, char *argv[]) {
 				case SDL_KEYUP:
 					xspeed = 0;
 					yspeed = 0;
+					steps = 0;
+					frame = 0;
 					break;
 			}
 		}
 
-		if(xspeed == 1) playerBox.x += 1;
+		if(xspeed == 1)
+		{
+			playerBox.x += 1;
+			steps +=1;
+			if(steps > BLOCK_SIZE*8) steps = 0;
+			frame = steps/(BLOCK_SIZE*8/6) + 1;
+		}
 		if(xspeed == -1) playerBox.x -= 1;
 		if(yspeed == 1) playerBox.y -= 1;
 		if(yspeed == -1) playerBox.y += 1;
 
+		SDL_Rect FrameBox;
+		FrameBox.x = 100*frame;
+		FrameBox.y = 0;
+		FrameBox.h = 100;
+		FrameBox.w = 100;
 
 		SDL_RenderClear(rend);
 		SDL_RenderCopy(rend, background, NULL, NULL);
-		while(g_map->current != NULL)
+		//printf("Start render\n");
+		for(int i=0;i<g_map->height;i++)
 		{
-			SDL_RenderCopy(rend, g_map->current->texture, NULL, &g_map->current->box);
-			nextBlock(g_map);
+			for(int j=0;j<g_map->width;j++)
+			{
+				//printf("Block %i \n", i*g_map->width + j);
+				MapField f = g_map->fields[i*g_map->width + j];
+				if(f.head != NULL) SDL_RenderCopy(rend, f.current->texture, NULL, &f.current->box);
+			}
 		}
-		headBlock(g_map);
-		SDL_RenderCopy(rend, player, NULL, &playerBox);
+		SDL_RenderCopy(rend, player, &FrameBox, &playerBox);
 		SDL_RenderPresent(rend);
 		SDL_Delay(1);
 	}
---
+++ like_a_mushroom/src/map_funcs.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include <SDL_image.h>
+
